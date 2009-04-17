34c56729a88f4cff7325dc09af377621e7035ee6
Jake Douglas <jakecdouglas@gmail.com>
2009-04-17 08:46:43 -0700

    Kqueue.new, #add_file, #add_socket#poll

--- lib/ktools/kqueue.rb
+++ lib/ktools/kqueue.rb
@@ -1,5 +1,6 @@
 module Kernel
-  module Kqueue
+
+  class Kqueue
     extend FFI::Library
 
     class Kevent < FFI::Struct
@@ -9,6 +10,11 @@ module Kernel
       :fflags, :uint, 
       :data, :int, 
       :udata, :pointer
+    end
+
+    class Timespec < FFI::Struct
+      layout :tv_sec, :long,
+        :tv_nsec, :long
     end
 
     kqc = FFI::ConstGenerator.new do |c|
@@ -63,11 +69,151 @@ module Kernel
 
     eval kqc.to_ruby
 
+    KQ_FILTERS = {
+      :read => EVFILT_READ,
+      :write => EVFILT_WRITE,
+      :file => EVFILT_VNODE,
+      :process => EVFILT_PROC,
+      :signal => EVFILT_SIGNAL
+    }
+
+    KQ_FLAGS = {
+      :add => EV_ADD,
+      :enable => EV_ENABLE,
+      :disable => EV_DISABLE,
+      :delete => EV_DELETE,
+      :oneshot => EV_ONESHOT,
+      :clear => EV_CLEAR
+    }
+
+    KQ_FFLAGS = {
+      :delete => NOTE_DELETE,
+      :write => NOTE_WRITE,
+      :extend => NOTE_EXTEND,
+      :attributes => NOTE_ATTRIB,
+      :link => NOTE_LINK,
+      :rename => NOTE_RENAME,
+      :revoke => NOTE_REVOKE,
+      :exit => NOTE_EXIT,
+      :fork => NOTE_FORK,
+      :exec => NOTE_EXEC
+    }
+
+    # Leopard has these, Tiger does not.
+    KQ_FLAGS[:receipt] = EV_RECEIPT if const_defined?("EV_RECEIPT")
+    KQ_FFLAGS[:signal] = NOTE_SIGNAL if const_defined?("NOTE_SIGNAL")
+    KQ_FFLAGS[:reap] = NOTE_REAP if const_defined?("NOTE_REAP")
+
     # Had to write a wrapper for EV_SET since its a macro
     attach_function :ev_set, :wrap_evset, [:pointer, :uint, :short, :ushort, :uint, :int, :pointer], :void
     # Attach directly to kqueue function, no wrapper needed
     attach_function :kqueue, [], :int
     # We wrap kqueue and kevent because we use rb_thread_blocking_region when it's available in MRI
-    attach_function :kevent, :wrap_kevent, [:int, :pointer, :int, :pointer, :int, :pointer], :int
+    attach_function :kevent, :wrap_kevent, [:int, :pointer, :int, :pointer, :int, Timespec], :int
+
+    # We provide the raw C interface above. Now we OO-ify it.
+
+    def initialize
+      @fds = {}
+      @kqfd = kqueue
+      raise SystemCallError.new("Error creating kqueue descriptor", get_errno) unless @kqfd > 0
+    end
+
+    def add_file(file, options={})
+      fflags, flags = options.values_at :events, :flags
+      raise ArgumentError.new("must specify which file events to watch for") unless fflags
+
+      k = Kevent.new
+      flags = flags ? flags.inject(0){|m,i| m | KQ_FLAGS[i] } : EV_CLEAR
+      fflags = fflags.inject(0){|m,i| m | KQ_FFLAGS[i] }
+      ev_set(k, file.fileno, EVFILT_VNODE, EV_ADD | flags, fflags, 0, nil)
+
+      if kevent(@kqfd, k, 1, nil, 0, nil) == -1
+        return false
+      else
+        @fds[file.fileno] = {:target => file, :kevent => k}
+        return true
+      end
+    end
+    
+    def add_socket(io, options={})
+      filters, flags = options.values_at :events, :flags
+      flags = flags ? flags.inject(0){|m,i| m | KQ_FLAGS[i] } : EV_CLEAR
+      filters = filters ? filters.inject(0){|m,i| m | KQ_FILTERS[i] } : EVFILT_READ | EVFILT_WRITE
+
+      k = Kevent.new
+      ev_set(k, io.fileno, filters, EV_ADD | flags, 0, 0, nil)
+      
+      if kevent(@kqfd, k, 1, nil, 0, nil) == -1
+        return false
+      else
+        @fds[io.fileno] = {:target => io, :kevent => k}
+        return true
+      end
+    end
+
+    # Pass an optional timeout
+    def poll(timeout=0)
+      k = Kevent.new
+      t = Timespec.new
+      t[:tv_sec] = timeout
+      case kevent(@kqfd, nil, 0, k, 1, t)
+      when -1
+        [errno]
+      when 0
+        []
+      else
+        process_event(k)
+      end
+    end
+
+    def process_event(k)
+      h = @fds[k[:ident]]
+      return nil if h.nil?
+      res = {:target => h[:target]}
+
+      case k[:filter]
+      when EVFILT_VNODE
+        event = if k[:fflags] & NOTE_DELETE == NOTE_DELETE
+          :delete
+        elsif k[:fflags] & NOTE_WRITE == NOTE_WRITE
+          :write
+        elsif k[:fflags] & NOTE_EXTEND == NOTE_EXTEND
+          :extend
+        elsif k[:fflags] & NOTE_ATTRIB == NOTE_ATTRIB
+          :attributes
+        elsif k[:fflags] & NOTE_LINK == NOTE_LINK
+          :link
+        elsif k[:fflags] & NOTE_RENAME == NOTE_RENAME
+          :rename
+        elsif k[:fflags] & NOTE_REVOKE == NOTE_REVOKE
+          :revoke
+        end
+        res.merge!({:type => :file, :event => event})
+      when EVFILT_READ
+        res.merge!({:type => :socket, :event => :read})
+      when EVFILT_WRITE
+        res.merge!({:type => :socket, :event => :write})
+      end
+
+      @fds.delete(k[:ident]) if k[:flags] & EV_ONESHOT == EV_ONESHOT
+      [res]
+    end
+
+    # Delete all events for this IO object
+    def delete(io)
+      h = @fds[io.fileno]
+      return nil if h.nil?
+      k = h[:kevent]
+      ev_set(k, k[:ident], k[:filter], EV_DELETE, k[:fflags], 0, nil)
+      kevent(@kqfd, k, 1, nil, 0, nil)
+      @fds.delete(io.fileno)
+      return io
+    end
+
+    def close
+      IO.for_fd(@kqfd).close
+    end
+
   end
 end
--- tests/test_kqueue.rb
+++ tests/test_kqueue.rb
@@ -1,29 +1,27 @@
 require 'lib/ktools'
 require 'bacon'
 
-include Kernel::Kqueue
-
 describe "the kqueue interface" do
 
   it "should return a valid kqueue file descriptor" do
-    @kqfd = kqueue
+    @kqfd = Kqueue::kqueue
     @kqfd.class.should.equal Fixnum
     @kqfd.should.be > 0
   end
 
-  it "should set an event for file watching, and retrieve the event when it occurs" do
+  it "should use the raw C API to set an event for file watching, and retrieve the event when it occurs" do
     file = Tempfile.new("kqueue-test")
-    k = Kevent.new
+    k = Kqueue::Kevent.new
     k.should.not.be.nil
-    ev_set(k, file.fileno, EVFILT_VNODE, EV_ADD | EV_ONESHOT, NOTE_DELETE | NOTE_RENAME | NOTE_WRITE, 0, nil)
-    kevent(@kqfd, k, 1, nil, 0, nil)
+    Kqueue::ev_set(k, file.fileno, Kqueue::EVFILT_VNODE, Kqueue::EV_ADD | Kqueue::EV_CLEAR, Kqueue::NOTE_WRITE, 0, nil)
+    Kqueue::kevent(@kqfd, k, 1, nil, 0, nil)
     File.open(file.path, 'w'){|x| x.puts 'foo'}
-    n = Kevent.new
-    res = kevent(@kqfd, nil, 0, n, 1, nil)
+    n = Kqueue::Kevent.new
+    res = Kqueue::kevent(@kqfd, nil, 0, n, 1, nil)
     res.should.be > -1
     n[:ident].should.equal file.fileno
-    n[:filter].should.equal EVFILT_VNODE
-    n[:fflags].should.equal NOTE_WRITE
+    n[:filter].should.equal Kqueue::EVFILT_VNODE
+    n[:fflags].should.equal Kqueue::NOTE_WRITE
     file.close
   end
 
@@ -32,6 +30,56 @@ describe "the kqueue interface" do
     i.should.not.be.closed
     i.close
     i.should.be.closed
+  end
+
+  it "should add file events using the ruby API" do
+    file = Tempfile.new("kqueue-test")
+    kq = Kqueue.new
+    kq.add_file(file, :events => [:write, :delete]).should.be.true
+
+    kq.poll.should.be.empty
+    File.open(file.path, 'w'){|x| x.puts 'foo'}
+
+    res = kq.poll.first
+    res.class.should.equal Hash
+    res[:target].class.should.equal Tempfile
+    res[:target].fileno.should.equal file.fileno
+    res[:type].should.equal :file
+    res[:event].should.equal :write
+
+    kq.poll.should.be.empty
+    file.delete
+
+    res2 = kq.poll.first
+    res2[:target].class.should.equal Tempfile
+    res2[:target].fileno.should.equal file.fileno
+    res2[:type].should.equal :file
+    res2[:event].should.equal :delete
+
+    file.close
+    kq.close
+  end
+
+  it "should add events for socket-style descriptors, then delete them, using the Ruby API" do
+    r, w = IO.pipe
+    kq = Kqueue.new
+    kq.add_socket(r, :events => [:read]).should.be.true
+
+    kq.poll.should.be.empty
+    w.write "foo"
+
+    res = kq.poll.first
+    res[:target].class.should.equal IO
+    res[:target].fileno.should.equal r.fileno
+    res[:type].should.equal :socket
+    res[:event].should.equal :read
+
+    kq.poll.should.be.empty
+    kq.delete(r)
+    w.write "foo"
+    kq.poll.should.be.empty
+
+    [r,w,kq].each{|i| i.close}
   end
 
 end
